2fc22649ef2a3ca1e07127644910abf2ebf49614
DimensionalX <50849877+leAdmin666@users.noreply.github.com>
2023-12-16 16:24:39 +0100

    DXFramework v1: Base Window for Windows (SDL2)

--- src/windows/DXFramework.cpp
+++ src/windows/DXFramework.cpp
@@ -1,7 +1,29 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 #include <iostream>
 
 int main() {
-    std::cout << "test" << std::endl;
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        return -1;
+    }
+    
+    SDL_Window* window = SDL_CreateWindow("DXFramework", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1240, 720, SDL_WINDOW_OPENGL);
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+    bool close = false;
+    while(!close) {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            switch(event.type) {
+                case SDL_QUIT: {
+                    close = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    SDL_Quit();
 
     return 0;
 }
