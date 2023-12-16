d20cef8d472a6f56a36090fd4bc721fa8197d2cd
DimensionalX <50849877+leAdmin666@users.noreply.github.com>
2023-12-16 16:34:22 +0100

    change the module path for SDL2

--- src/windows/DXFramework.cpp
+++ src/windows/DXFramework.cpp
@@ -2,10 +2,30 @@
 #include <SDL.h>
 #include <iostream>
 
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef float float32;
+typedef double float64;
+
 int main() {
     if(SDL_Init(SDL_INIT_VIDEO) != 0) {
         return -1;
     }
+
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     
     SDL_Window* window = SDL_CreateWindow("DXFramework", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1240, 720, SDL_WINDOW_OPENGL);
     SDL_GLContext glContext = SDL_GL_CreateContext(window);
@@ -21,6 +41,8 @@ int main() {
                 }
             }
         }
+
+        
     }
 
     SDL_Quit();
